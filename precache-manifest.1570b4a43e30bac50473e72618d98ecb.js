505b43259a329a926d1ab7f86f3856cf6a931e36
Kaka <potatokaka@gmail.com>
2019-09-16 23:57:05 +0800

    Updates

---
+++ precache-manifest.1570b4a43e30bac50473e72618d98ecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea130c68d25f128885d417ab9f265b4",
+    "url": "/blog_react/index.html"
+  },
+  {
+    "revision": "53196887a77f708677cd",
+    "url": "/blog_react/static/css/main.f796042d.chunk.css"
+  },
+  {
+    "revision": "695fa8c8d2e797a4cebe",
+    "url": "/blog_react/static/js/2.2d480356.chunk.js"
+  },
+  {
+    "revision": "53196887a77f708677cd",
+    "url": "/blog_react/static/js/main.bfa104ae.chunk.js"
+  },
+  {
+    "revision": "4decc6dfdb46655930c1",
+    "url": "/blog_react/static/js/runtime~main.eae5af06.js"
+  }
+]);
